1d6745c568600975b42d87d6afd08e94b8f6297f
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-27 19:14:42 +0600

    added contact us form in about page

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+
+import ContactForm from '../components/ContactForm';
+
+const About = () => {
+  return (
+    <div>
+      <ContactForm></ContactForm>
+    </div>
+  );
+};
+
+export default About;
