797d71d9c95e26ec280fb088a6319f42c7565a3e
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-28 15:46:55 +0600

    updated select tag for sorting by category

--- src/Pages/ListedBooks.jsx
+++ src/Pages/ListedBooks.jsx
@@ -1,64 +1,62 @@
-import { IoIosArrowDown } from "react-icons/io";
 import ListedBook from "../components/ListedBook";
 
 import useFilterBook from "../Hooks/useFilterBook";
 import { useState } from "react";
 
 const ListedBooks = () => {
-
   const wishList = useFilterBook("wishList");
   const readingList = useFilterBook("readingList");
-  
 
   const [wishListDisplayData, setWishListDisplayData] = useState([]);
   const [readingListDisplayData, setReadingListDisplayData] = useState([]);
-  const [selectedTab, setSelectedTab] = useState('read');
+  const [selectedTab, setSelectedTab] = useState("read");
 
-
-  const sortData = (sortCategory,bookDatas) => {
+  const sortData = (sortCategory, bookDatas) => {
     // let sortedData;
-    console.log(sortCategory)
-    if(sortCategory && bookDatas){
-      const sortedData = [...bookDatas].sort((prev,curr)=> curr[sortCategory] - prev[sortCategory] )
-      return sortedData
+    console.log(sortCategory);
+    if (sortCategory && bookDatas) {
+      const sortedData = [...bookDatas].sort(
+        (prev, curr) => curr[sortCategory] - prev[sortCategory]
+      );
+      return sortedData;
     }
   };
 
-
   const sort = (rating) => {
     const newWishListData = sortData(rating, wishList);
-    setWishListDisplayData([...newWishListData])
+    setWishListDisplayData([...newWishListData]);
     const newReadListData = sortData(rating, readingList);
-    setReadingListDisplayData([...newReadListData])
+    setReadingListDisplayData([...newReadListData]);
   };
 
-  const handleTab=(tabName)=>{
+  const handleSortChange = (e) => {
+    const selectedValue = e.target.value;
+    sort(selectedValue); 
+  };
+  const handleTab = (tabName) => {
     setSelectedTab(tabName);
-  }
-
+  };
 
   return (
     <div>
       <div className="py-5 md:py-8 w-full bg-base-200 rounded-xl">
         <h1 className="text-center font-bold text-2xl lg:text-3xl">Books</h1>
       </div>
+
       <div className="flex justify-center my-4 md:my-6">
-        <details className="dropdown">
-          <summary className="m-1 btn bg-c-primary text-white hover:bg-white hover:border-c-primary hover:text-c-primary">
-            Sort By <IoIosArrowDown></IoIosArrowDown>
-          </summary>
-          <ul className="p-2 shadow menu dropdown-content z-[1] rounded-box w-40 bg-base-200">
-            <li>
-              <a onClick={() => sort("rating")}>Rating</a>
-            </li>
-            <li>
-              <a onClick={() => sort("totalPages")}>Number of pages</a>
-            </li>
-            <li>
-              <a onClick={() => sort("yearOfPublishing")}>Published year</a>
-            </li>
-          </ul>
-        </details>
+        <select onChange={handleSortChange} className="select bg-c-primary text-white hover:bg-white hover:border-c-primary hover:text-c-primary font-bold focus:outline-none">
+          <option disabled selected className="hidden py-10">
+            Sort By
+          </option>
+          <option value="rating" className="text-lg  py-10 text-black bg-base-200 px-10">
+            Rating
+          </option>
+          <option value="totalPages" className="text-lg  py-10 text-black bg-base-200 px-10">Number of Pages
+          </option>
+          <option value="yearOfPublishing" className="text-lg  py-10 text-black bg-base-200 px-10">
+            Published year
+          </option>
+        </select>
       </div>
       <div>
         <div role="tablist" className="tabs tabs-lifted">
@@ -68,18 +66,20 @@ const ListedBooks = () => {
             role="tab"
             className="tab"
             aria-label="Read&nbsp;Books"
-            onChange={() => handleTab('read')}
-            checked={selectedTab === 'read'}
+            onChange={() => handleTab("read")}
+            checked={selectedTab === "read"}
           />
           <div
             role="tabpanel"
             className="tab-content bg-base-100 border-base-300  py-4 md:py-6 border-x-0 border-b-0"
           >
-            {readingListDisplayData.length>0?readingListDisplayData.map((book) => (
-              <ListedBook key={book.bookId} book={book}></ListedBook>
-            )):readingList.map((book) => (
-              <ListedBook key={book.bookId} book={book}></ListedBook>
-            ))}
+            {readingListDisplayData.length > 0
+              ? readingListDisplayData.map((book) => (
+                  <ListedBook key={book.bookId} book={book}></ListedBook>
+                ))
+              : readingList.map((book) => (
+                  <ListedBook key={book.bookId} book={book}></ListedBook>
+                ))}
           </div>
 
           <input
@@ -88,18 +88,20 @@ const ListedBooks = () => {
             role="tab"
             className="tab"
             aria-label="Wishlist&nbsp;Books"
-            onChange={() => handleTab('wish')}
-            checked={selectedTab === 'wish'}
+            onChange={() => handleTab("wish")}
+            checked={selectedTab === "wish"}
           />
           <div
             role="tabpanel"
             className="tab-content bg-base-100 border-base-300  py-4 md:py-6 border-x-0 border-b-0"
           >
-            {wishListDisplayData.length>0?wishListDisplayData.map((book) => (
-              <ListedBook key={book.bookId} book={book}></ListedBook>
-            )):wishList.map((book) => (
-              <ListedBook key={book.bookId} book={book}></ListedBook>
-            ))}
+            {wishListDisplayData.length > 0
+              ? wishListDisplayData.map((book) => (
+                  <ListedBook key={book.bookId} book={book}></ListedBook>
+                ))
+              : wishList.map((book) => (
+                  <ListedBook key={book.bookId} book={book}></ListedBook>
+                ))}
           </div>
         </div>
       </div>
