043b08b77dbc345084e3ea571b2b34a3faab310d
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-28 12:59:39 +0600

    added path for about page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root/Root';
+import Home from './Pages/Home';
+import BookDetails from './Pages/BookDetails';
+import ListedBooks from './Pages/ListedBooks';
+import PagesToRead from './Pages/PagesToRead';
+import ErrorPage from './Pages/ErrorPage';
+import Blogs from './Pages/Blogs';
+import About from './Pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/book/:id",
+        element: <BookDetails></BookDetails>
+      },
+      {
+        path: "/listed",
+        element: <ListedBooks></ListedBooks>
+      },
+      {
+        path:"/blogs",
+        loader: ()=> fetch('../blogs.json'),
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "/pages",
+        element: <PagesToRead></PagesToRead>
+      },
+      {
+        path: "/about",
+        element: <About></About>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
