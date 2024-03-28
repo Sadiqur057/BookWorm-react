ee491695df5851e50c4c7a83a9452d1c74a21aa9
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-28 21:28:40 +0600

    configured state to set loading while fetching data

--- src/Hooks/useFilterBook.jsx
+++ src/Hooks/useFilterBook.jsx
@@ -2,20 +2,24 @@ import { useEffect, useState } from "react";
 import useLoadBookData from "../Hooks/useLoadBookData";
 import getData from "../utils/GetDataFromLS";
 
-
 const useFilterBook = (category) => {
+  const [loading, setLoading] = useState(false);
   const [readingBook, setReadingBook] = useState([]);
-
   const { bookData } = useLoadBookData();
 
-  useEffect(()=>{
+  useEffect(() => {
+    setLoading(true);
     const listBooksId = getData(category);
-    if(listBooksId.length>0){
-      const readBooks = bookData.filter(book=> listBooksId.includes(book.bookId))
-      setReadingBook(readBooks)
+    if (listBooksId.length > 0) {
+      const readBooks = bookData.filter((book) =>
+        listBooksId.includes(book.bookId)
+      );
+      setReadingBook(readBooks);
     }
-  },[bookData,category])
-  return readingBook
+
+    setLoading(false);
+  }, [bookData, category]);
+  return { readingBook, loading };
 };
 
 export default useFilterBook;
--- src/Hooks/useLoadBookData.jsx
+++ src/Hooks/useLoadBookData.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 
-
 const useLoadBookData = () => {
   const [bookData, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      const res = await fetch ('/bookData.json');
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      const res = await fetch("/bookData.json");
       const bookData = await res.json();
-      setData(bookData)
-    }
+      setData(bookData);
+    };
     fetchData();
-  },[])
+    setLoading(false);
+  }, []);
 
-  return {bookData}
+  return { bookData, loading };
 };
 
 export default useLoadBookData;
