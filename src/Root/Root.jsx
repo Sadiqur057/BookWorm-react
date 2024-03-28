a742d20b36cda425e773f8db5468c6547cbae265
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-28 12:57:31 +0600

    added toast container

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import { ToastContainer } from "react-toastify";
+
 
 const Root = () => {
   return (
     <>
       <NavBar></NavBar>
       <div className="md:my-4 w-[90%] md:w-5/6 lg:w-4/5 mx-auto">
+      <ToastContainer />
         <Outlet></Outlet>
       </div>
     </>
