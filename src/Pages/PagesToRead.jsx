c685cf59aa7167f1db6a4cb1ad66089ad0eb9f03
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-03-28 12:56:05 +0600

    added chart to show number of pages read

---
+++ src/Pages/PagesToRead.jsx
@@ -0,0 +1,75 @@
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Cell } from 'recharts';
+import useFilterBook from '../Hooks/useFilterBook';
+import { useEffect, useState } from 'react';
+
+
+const PagesToRead = () => {
+  const readingList = useFilterBook("readingList");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (readingList.length > 0) {
+      const newData = readingList.map(bookData => ({
+        bookName: bookData.bookName.slice(0,15)+"...",
+        page: bookData.totalPages
+      }));
+      setData(newData);
+    }
+
+  }, [readingList]);
+  console.log(data)
+
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+  
+
+
+const getPath = (x, y, width, height) => (
+  `M${x},${y + height}
+   C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
+   C${x + width / 2},${y + height / 3} ${x + 2 * width / 3},${y + height} ${x + width}, ${y + height}
+   Z`
+);
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+const renderCustomAxisTick = (props) => {
+  const { x, y, payload } = props;
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text x={0} y={0} dy={16} textAnchor="middle" fill="#666">{payload.value}</text>
+    </g>
+  );
+};
+console.log(data.length)
+if(data.length===0){
+  return (
+    <div className='bg-base-200 py-10 flex justify-center rounded-xl md:rounded-3xl'>
+      <h1 className='font-bold text-2xl md:text-3xl'>No Data found</h1>
+    </div>
+  )
+}
+
+return (
+  <div className='w-full mx-auto py-8 px-4 lg:py-20 lg:px-10 bg-base-200 overflow-auto rounded-xl md:rounded-3xl'>
+      <BarChart className='w-full mx-auto' width={650} height={300} data={data}>
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="bookName" tick={renderCustomAxisTick} />
+    <YAxis />
+    <Tooltip />
+    <Bar dataKey="page" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+        ))}
+      </Bar>
+  </BarChart>
+  </div>
+);
+
+}
+
+export default PagesToRead;
